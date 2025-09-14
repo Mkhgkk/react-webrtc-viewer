148810c38e265e9676114ad0f623c46f98c446b6
hyeyeah <hyeyeah0225@gmail.com>
2025-09-14 22:52:22 +0900

    chore: update package-lock.json and package.json for new dependencies and version bump

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,55 @@
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: [
     {
-      file: 'dist/index.js',
-      format: 'cjs',
+      file: "dist/index.js",
+      format: "cjs",
       sourcemap: true,
-      exports: 'auto'
+      exports: "auto",
     },
     {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true
-    }
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
   ],
   plugins: [
     peerDepsExternal(),
+    postcss({
+      extract: true,
+      minimize: true,
+    }),
     resolve({
       browser: true,
       preferBuiltins: false,
-      extensions: ['.js', '.jsx', '.ts', '.tsx']
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
     }),
     commonjs(),
     babel({
-      babelHelpers: 'runtime',
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      babelHelpers: "runtime",
+      exclude: "node_modules/**",
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
       presets: [
-        ['@babel/preset-env', { modules: false }],
-        ['@babel/preset-react', { runtime: 'automatic' }]
+        ["@babel/preset-env", { modules: false }],
+        ["@babel/preset-react", { runtime: "automatic" }],
       ],
       plugins: [
-        ['@babel/plugin-transform-runtime', {
-          useESModules: true
-        }]
-      ]
+        [
+          "@babel/plugin-transform-runtime",
+          {
+            useESModules: true,
+          },
+        ],
+      ],
     }),
-    terser()
+    terser(),
   ],
-  external: ['react', 'react-dom', 'video.js', 'whip-whep/whep.js']
+  external: ["react", "react-dom", "video.js", "whip-whep/whep.js"],
 };
